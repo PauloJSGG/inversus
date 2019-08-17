df1acb1c288a835d2183f2e996a854d933fce0e8
Paulo Goncalves <paulojsgg@gmail.com>
2019-08-17 14:07:51 +0100

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+  const Body = () => {
+  return(
+    <h1>Body</h1>
+  )
+}
+
+export default Body
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+  const Header = () => {
+  return(
+    <div className='header-container'>
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
