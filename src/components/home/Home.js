27f3d7ee8f6e40c1f48a153af19d014b065239c9
Paulo Goncalves <paulojsgg@gmail.com>
2019-08-23 01:42:45 +0100

    Add faded text

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,18 +2,26 @@ import React from 'react'
 
 const Home = () => {
   return (
+    <>
     <div className='home'>
-      <p className='home__text m-3'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin, turpis at lacinia auctor, leo lectus vulputate massa, id facilisis diam velit vitae magna. Proin tincidunt libero eget diam gravida, vel vehicula lacus finibus. In vehicula, risus id sodales sodales, urna justo imperdiet leo, laoreet facilisis ante ipsum non libero. Nam enim nisi, elementum a tempus eget, aliquet sit amet ex. Ut lobortis, elit a imperdiet vulputate, nulla augue lacinia lacus, in congue felis est eget sem. Aliquam pharetra euismod leo. Sed lacus sem, pretium id odio sit amet, pellentesque elementum nibh. Sed eros nulla, laoreet a lectus id, dignissim bibendum arcu. Phasellus id massa et magna mattis bibendum dapibus at magna. Pellentesque quam sem, egestas vitae tristique ut, dapibus eu mi.
+    <div className='shade1'></div>
+        <p className='home__text m-3'>
+          <br/>
+          <br/>
 
-        Mauris in vehicula erat. Phasellus sodales sapien a nisl porttitor, sit amet feugiat neque scelerisque. Donec ultrices sapien felis, a vestibulum erat sollicitudin ut. Nunc euismod, sapien et dapibus bibendum, neque quam pharetra tellus, vitae dapibus odio massa nec nulla. Donec nec auctor felis. Nam euismod elit non mi imperdiet, at sagittis lectus pulvinar. Etiam pretium sapien a metus venenatis, ac varius eros facilisis. Nullam lacus nisi, condimentum non neque non, fringilla luctus augue. In hac habitasse platea dictumst. Nulla scelerisque vitae justo non gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ut laoreet erat.
-      </p>
-      <p className='home__text m-3'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin, turpis at lacinia auctor, leo lectus vulputate massa, id facilisis diam velit vitae magna. Proin tincidunt libero eget diam gravida, vel vehicula lacus finibus. In vehicula, risus id sodales sodales, urna justo imperdiet leo, laoreet facilisis ante ipsum non libero. Nam enim nisi, elementum a tempus eget, aliquet sit amet ex. Ut lobortis, elit a imperdiet vulputate, nulla augue lacinia lacus, in congue felis est eget sem. Aliquam pharetra euismod leo. Sed lacus sem, pretium id odio sit amet, pellentesque elementum nibh. Sed eros nulla, laoreet a lectus id, dignissim bibendum arcu. Phasellus id massa et magna mattis bibendum dapibus at magna. Pellentesque quam sem, egestas vitae tristique ut, dapibus eu mi.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin, turpis at lacinia auctor, leo lectus vulputate massa, id facilisis diam velit vitae magna. Proin tincidunt libero eget diam gravida, vel vehicula lacus finibus. In vehicula, risus id sodales sodales, urna justo imperdiet leo, laoreet facilisis ante ipsum non libero. Nam enim nisi, elementum a tempus eget, aliquet sit amet ex. Ut lobortis, elit a imperdiet vulputate, nulla augue lacinia lacus, in congue felis est eget sem. Aliquam pharetra euismod leo. Sed lacus sem, pretium id odio sit amet, pellentesque elementum nibh. Sed eros nulla, laoreet a lectus id, dignissim bibendum arcu. Phasellus id massa et magna mattis bibendum dapibus at magna. Pellentesque quam sem, egestas vitae tristique ut, dapibus eu mi.
 
-        Mauris in vehicula erat. Phasellus sodales sapien a nisl porttitor, sit amet feugiat neque scelerisque. Donec ultrices sapien felis, a vestibulum erat sollicitudin ut. Nunc euismod, sapien et dapibus bibendum, neque quam pharetra tellus, vitae dapibus odio massa nec nulla. Donec nec auctor felis. Nam euismod elit non mi imperdiet, at sagittis lectus pulvinar. Etiam pretium sapien a metus venenatis, ac varius eros facilisis. Nullam lacus nisi, condimentum non neque non, fringilla luctus augue. In hac habitasse platea dictumst. Nulla scelerisque vitae justo non gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ut laoreet erat.
-      </p>
+          Mauris in vehicula erat. Phasellus sodales sapien a nisl porttitor, sit amet feugiat neque scelerisque. Donec ultrices sapien felis, a vestibulum erat sollicitudin ut. Nunc euismod, sapien et dapibus bibendum, neque quam pharetra tellus, vitae dapibus odio massa nec nulla. Donec nec auctor felis. Nam euismod elit non mi imperdiet, at sagittis lectus pulvinar. Etiam pretium sapien a metus venenatis, ac varius eros facilisis. Nullam lacus nisi, condimentum non neque non, fringilla luctus augue. In hac habitasse platea dictumst. Nulla scelerisque vitae justo non gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ut laoreet erat.
+
+
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin, turpis at lacinia auctor, leo lectus vulputate massa, id facilisis diam velit vitae magna. Proin tincidunt libero eget diam gravida, vel vehicula lacus finibus. In vehicula, risus id sodales sodales, urna justo imperdiet leo, laoreet facilisis ante ipsum non libero. Nam enim nisi, elementum a tempus eget, aliquet sit amet ex. Ut lobortis, elit a imperdiet vulputate, nulla augue lacinia lacus, in congue felis est eget sem. Aliquam pharetra euismod leo. Sed lacus sem, pretium id odio sit amet, pellentesque elementum nibh. Sed eros nulla, laoreet a lectus id, dignissim bibendum arcu. Phasellus id massa et magna mattis bibendum dapibus at magna. Pellentesque quam sem, egestas vitae tristique ut, dapibus eu mi.
+
+          Mauris in vehicula erat. Phasellus sodales sapien a nisl porttitor, sit amet feugiat neque scelerisque. Donec ultrices sapien felis, a vestibulum erat sollicitudin ut. Nunc euismod, sapien et dapibus bibendum, neque quam pharetra tellus, vitae dapibus odio massa nec nulla. Donec nec auctor felis. Nam euismod elit non mi imperdiet, at sagittis lectus pulvinar. Etiam pretium sapien a metus venenatis, ac varius eros facilisis. Nullam lacus nisi, condimentum non neque non, fringilla luctus augue. In hac habitasse platea dictumst. Nulla scelerisque vitae justo non gravida. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ut laoreet erat.
+        </p>
+        <div className='shade2'></div>
     </div>
+
+    </>
 
   )
 }
