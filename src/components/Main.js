412413ded70e8a94af4da8f02def884afde09f9d
Paulo Goncalves <paulojsgg@gmail.com>
2019-08-25 16:26:14 +0100

    Add transition css

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,10 @@ import Footer from './Footer'
 import Social from './social/Social'
 import Events from './events/Events'
 import Home from './home/Home'
+import {
+  CSSTransition,
+  TransitionGroup
+} from 'react-transition-group'
 
 import { Route, Switch } from 'react-router-dom'
 
@@ -14,12 +18,21 @@ class Main extends Component {
     return (
       <>
         <Header/>
-          <Switch>
-            <Route path='/social' component={Social}/>
-            <Route path='/events' component={Events}/>
-            <Route path='/social' component={Social}/>
-            <Route path='/' component={Home}/>
-          </Switch>
+        <Route render={({location}) => (
+          <TransitionGroup>
+           <CSSTransition
+            key={location.key}
+            timeout={300}
+            classNames="fade">
+             <Switch>
+               <Route path='/social' component={Social}/>
+               <Route path='/events' component={Events}/>
+               <Route path='/social' component={Social}/>
+               <Route path='/' component={Home}/>
+             </Switch>
+           </CSSTransition>
+         </TransitionGroup>
+        )} />
         <Footer/>
       </>
     )
