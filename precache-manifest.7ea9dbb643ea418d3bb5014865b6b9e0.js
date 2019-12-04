c2af38241276b3032a4a0651bb87223cf70da7dc
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2019-12-04 17:01:23 +0000

    Updates

---
+++ precache-manifest.7ea9dbb643ea418d3bb5014865b6b9e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235f6e2badf9687d27364ec80d1d88da",
+    "url": "/inverus/index.html"
+  },
+  {
+    "revision": "80c911deba9f6a78e742",
+    "url": "/inverus/static/css/main.1265900f.chunk.css"
+  },
+  {
+    "revision": "8d4858c0bf021cf4ca3e",
+    "url": "/inverus/static/js/2.17076f5d.chunk.js"
+  },
+  {
+    "revision": "80c911deba9f6a78e742",
+    "url": "/inverus/static/js/main.a6ec7820.chunk.js"
+  },
+  {
+    "revision": "84f9ab2624bd9771fda1",
+    "url": "/inverus/static/js/runtime~main.32c70fc5.js"
+  },
+  {
+    "revision": "8ae6123dcddab7edfef96f93339a88aa",
+    "url": "/inverus/static/media/FADO.8ae6123d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inverus/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "504f93d3c505773d913898f6149dd29a",
+    "url": "/inverus/static/media/logo_black_small.504f93d3.png"
+  }
+]);
