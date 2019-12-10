9be6efeabf4f69de4de41ca7a181f13c5c8631a0
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2019-12-10 15:51:09 +0000

    Change manifest

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './assets/css/index.scss';
 // import './vendor/css/index.css';
 
 import MainRoute from './routes/MainRoute'
 import AdminRoute from './routes/AdminRoute'
-import Auth from './containers/AuthContainer'
+// import Auth from './containers/AuthContainer'
 
 import LoginContainer from './containers/LoginContainer'
 
@@ -13,9 +13,9 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom'
 // import { Redirect, Switch } from 'react-router-dom'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fas, faHome, faMapMarker } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { fas, faHome, faMapMarker } from '@fortawesome/free-solid-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fab, fas)
 
@@ -25,7 +25,6 @@ function App() {
     <>
       <div className="global-container">
         <BrowserRouter>
-          {/* <Redirect from="/" exact to="/main" /> */}
           <Switch>
             <Route path='/admin' component={AdminRoute} />
             <Route path='/login' exact component={LoginContainer} />
