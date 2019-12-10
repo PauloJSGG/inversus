6299acaa501536812de55bdf4b524f72162a0440
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2019-12-10 13:29:03 +0000

    Updates

---
+++ precache-manifest.7c2f38473b7895bc4527190e4ff1f612.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362098818b8a7be5ae2af266f3e832b8",
+    "url": "/inversus/index.html"
+  },
+  {
+    "revision": "8e92f240745907482606",
+    "url": "/inversus/static/css/main.050600fd.chunk.css"
+  },
+  {
+    "revision": "5a655424b7c67ebf4400",
+    "url": "/inversus/static/js/2.f2d4f1c4.chunk.js"
+  },
+  {
+    "revision": "8e92f240745907482606",
+    "url": "/inversus/static/js/main.5f01f2ee.chunk.js"
+  },
+  {
+    "revision": "55e16c6f8fe3e4dc588e",
+    "url": "/inversus/static/js/runtime~main.8a87e9e9.js"
+  },
+  {
+    "revision": "8ae6123dcddab7edfef96f93339a88aa",
+    "url": "/inversus/static/media/FADO.8ae6123d.jpg"
+  },
+  {
+    "revision": "69ccebc79b1b1a91264cd7db692525e8",
+    "url": "/inversus/static/media/de.69ccebc7.svg"
+  },
+  {
+    "revision": "c4cd6642c20f6473ef5b38fbf9c3aa11",
+    "url": "/inversus/static/media/gb.c4cd6642.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/inversus/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "504f93d3c505773d913898f6149dd29a",
+    "url": "/inversus/static/media/logo_black_small.504f93d3.png"
+  },
+  {
+    "revision": "8ce156e00aa8ae4407eb5c1efae8f5fd",
+    "url": "/inversus/static/media/pt.8ce156e0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc6128035e721d087b0336516e20379d.js"
+  "/inversus/precache-manifest.7c2f38473b7895bc4527190e4ff1f612.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/inversus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
