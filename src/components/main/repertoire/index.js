e1ae15b009389c0129f316c90965222e94bbeaf8
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2019-12-10 22:42:37 +0000

    Updates to repertoire

--- src/components/main/repertoire/index.js
+++ src/components/main/repertoire/index.js
@@ -12,17 +12,21 @@ export const repertoire = (props) => {
     <div>
       <div className = 'flex flex-row w-full'>
         <Divider classs='test test--flip' />
+        <h2 style = {{color: 'white'}} className = 'my-20'>Repertoio</h2>
         <Divider classs='test'  />
       </div>
       {/* <svg src = {divider}/> */}
-      {repertoire.map(item => {
-        return(
-          <div className = "repertoire">
-            <img className = "repertoire__track-logo" src = {item.data.imgUrl === "" ? guitarImg : item.data.imgUrl } alt = "track"/>
-            <h1>{item.data.name}</h1>
-          </div>
-        )
-      })}
+      <div className = 'flex flex-row flex-wrap w-full'>
+        {repertoire.map(item => {
+          return(
+
+            <div className = "repetoire-card">
+              {/* <img className = "repertoire__track-logo" src = {item.data.imgUrl === "" ? guitarImg : item.data.imgUrl } alt = "track"/> */}
+              <h1>{item.data.name}</h1>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
