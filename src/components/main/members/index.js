012317f503dd7b7c2d4ca05bdebd892ac7a558e6
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2020-01-03 04:51:55 +0000

    Add parallax v1

--- src/components/main/members/index.js
+++ src/components/main/members/index.js
@@ -1,25 +1,36 @@
 import React from 'react'
 
 const Members = (props) => {
-  debugger
   const {dynamicData} = props
 
-
-
   return (
-    <div className="content-container">
-      {
-        dynamicData.members.map((data, i) => {
-          return(
-            <>
-              <img src={data.src}/>
-              <p>{data.text}</p>
-            </>
-          )
-        })
-      }
+    <div className="content-container content-container--no-flex">
+      <div className="parallax">
+        {
+          dynamicData.members.map((item, i) => {
+            const leftOrRight = i % 2 == 0 ? 'parallax--left' : 'parallax--right';
+            const color = leftOrRight == 'members--left' ? 'red' : 'green'
+            return(
+              <div className="parallax__group" id={'group_'.concat(i.toString())} >
+                <div className={leftOrRight.concat(" parallax__layer parallax__layer--base")}>
+                  <p className="members__text">{item.data.text}</p>
+                </div>
+                <div className={leftOrRight.concat(" parallax__layer parallax__layer--back")}>
+                  <img src={item.data.imgUrl} className="members__img"/>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
 
 export default Members;
+
+
+{/* <div className={leftOrRight.concat(' members')}>
+  <img src={item.data.imgUrl} className="members__img"/>
+  <p className="members__text">{item.data.text}</p>
+</div> */}
