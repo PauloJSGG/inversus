324e85599eb5fb37ddf3e4a7b94b34dc96c7c976
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2020-02-17 22:59:42 +0000

    Add style to albums

--- src/components/main/albums/index.js
+++ src/components/main/albums/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import capa from '../../../assets/img/albums/capa.png'
+import contracapa from '../../../assets/img/albums/contracapa.png'
+import cd from '../../../assets/img/albums/cd.png'
 
 const Albums = (props) => {
 
 
   return (
     <div className = "content-container">
-      <h1>hey</h1>
+      <div className="albums">
+        <div className="shade1"/>
+        <div className="albums-container">
+          <img src={capa}/>
+          <img src={contracapa}/>
+          <img src={cd}/>
+        </div>
+        <div className="shade2"/>
+      </div>
     </div>
   )
 }
