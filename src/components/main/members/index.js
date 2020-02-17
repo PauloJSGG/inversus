e16d18999ccc466149a16d729090f9cc7a542ce2
Paulo Gonçalves <paulo.goncalves@runtime-revolution.com>
2020-02-17 20:20:46 +0000

    Add style to members

--- src/components/main/members/index.js
+++ src/components/main/members/index.js
@@ -4,26 +4,24 @@ const Members = (props) => {
   const {dynamicData} = props
 
   return (
-    <div className="content-container content-container--no-flex">
-      <div className='shade1'/>
-      <div className="parallax">
-        {
-          dynamicData.members.map((item, i) => {
-            const leftOrRight = i % 2 === 0 ? 'left' : 'right';
-            return(
-              <div className={'parallax__group--'.concat(leftOrRight)}>
-                <div className="parallax__layer parallax__layer--back">
-                  <img src={item.imgUrl} className="members__img" alt="paralax"/>
-                </div>
-                <div className=" parallax__layer parallax__layer--base">
-                  <p className="members__text whitespace-pre-wrap">{item.text}</p>
-                </div>
-              </div>
-            )
-          })
-        }
+    <div className="content-container">
+      <div className="members">
+        <div className='shade1'/>
+          <div className="members-container">
+            {
+              dynamicData.members.map((item, i) => {
+                const leftOrRight = i % 2 === 0 ? 'left' : 'right';
+                return(
+                  <div>
+                    <img src={item.imgUrl} className="members__img" alt="paralax"/>
+                    <p className="members__text whitespace-pre-wrap">{item.text}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
+        <div className='shade2'/>
       </div>
-      <div className='shade2'/>
     </div>
   )
 }
